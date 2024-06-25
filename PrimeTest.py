e5c3315ce6215f2407c0abbd026f7fd2a00c46ea
Jonathan Fierstein <jonathan.fierstein@gmail.com>
2024-06-24 18:49:10 -0700

    Minor Code Update

--- PrimeTest.py
+++ PrimeTest.py
@@ -31,15 +31,9 @@ def find_primes(last_number):
       primes.append(num)
   return primes
 
-"""
-if __name__ == "__main__":
-  try:
-    last_number = int(input("Enter the last number to test (or press Enter to use 100): "))
-  except ValueError:
-    last_number = 100  # Default value if input is invalid
-  primes = find_primes(last_number)
-  print("Prime numbers found between 1 and", last_number, "are:", primes)
-"""
-last_number = 1000000000 #The number to input into find_primes
+
+last_number = 100000000#The number to input into find_primes
 primes = find_primes(last_number)
+numberofprimes = len(primes)
 print("Prime numbers found between 1 and", last_number, "are:", primes)
+print("A total of ", numberofprimes, " prime numbers were found between 1 and ", last_number, ".")
